1b852c8c0c2e7e456a8d0a4f7ae0a10e4cba7e08
dev-rne <coco.wapplab@gmail.com>
2022-08-26 17:07:30 +0900

    ⚡️ Add err handling, nunjucks

--- app.js
+++ app.js
@@ -21,11 +21,25 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/uploads', express.static('uploads'))
 
+app.use((req,res,next) => {
+    app.locals.isLogin = true;
+    app.locals.req_path = req.path;
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('hello express!');
 })
 
 app.use('/admin', admin);
+
+app.use((req,res,_) => {
+    res.status(400).render('common/404.html')
+})
+app.use((req,res,_) => {
+    res.status(500).render('common/500.html')
+})
+
 app.use('/contacts', contacts);
 
 app.listen(port, () => {
